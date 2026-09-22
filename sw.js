85e56401ba9e510110a2c2c1e38562db33785311
ScarterDM <156226590+ScarterDM@users.noreply.github.com>
2026-09-22 14:38:35 -0300

    Bloquear completamente o modo vertical

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='44433-pwa-v5';
+const CACHE='44433-pwa-v6';
 const FILES=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
